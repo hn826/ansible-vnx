f0806c4afda3638bd1e689f463c00509fbfa77fd
hn826 <hn826@users.noreply.github.com>
2016-06-01 14:09:13 +0900

    Add function of noremove

--- sglun-module/library/sglun.py
+++ sglun-module/library/sglun.py
@@ -6,8 +6,8 @@ import re
 from string import *
 from ansible.module_utils.basic import *
 
-def getPairs(sg):
-    (rc, out, err) = module.run_command('%s storagegroup -list -gname %s' % (naviseccli, sg), check_rc=True)
+def getPairs(gname):
+    (rc, out, err) = module.run_command('%s storagegroup -list -gname %s' % (naviseccli, gname), check_rc=True)
     retlist = []
     for l in re.split(r'\n', out):
         if re.search(r'^    .*', l):
@@ -17,19 +17,19 @@ def getPairs(sg):
 def parseQuery(query):
     return re.split(r':', query)
 
-def removeHlus(query,sg):
+def removeHlus(query,gname):
     for i in query:
-        pairs = getPairs(sg)
+        pairs = getPairs(gname)
         for j in pairs:
             if i != j:
                 hlu = (re.split(r',' , j))[0]
-                (rc, out, err) = module.run_command('%s storagegroup -removehlu -gname %s -hlu %s -o' % (naviseccli, sg, hlu), check_rc=True)
+                (rc, out, err) = module.run_command('%s storagegroup -removehlu -gname %s -hlu %s -o' % (naviseccli, gname, hlu), check_rc=True)
 
-def addHlus(query,sg):
+def addHlus(query,gname):
     for i in query:
         hlu = (re.split(r',' , i))[0]
         alu = (re.split(r',' , i))[1]
-        (rc, out, err) = module.run_command('%s storagegroup -addhlu -gname %s -hlu %s -alu %s' % (naviseccli, sg, hlu, alu), check_rc=True)
+        (rc, out, err) = module.run_command('%s storagegroup -addhlu -gname %s -hlu %s -alu %s' % (naviseccli, gname, hlu, alu), check_rc=True)
 
 def main():
     ### Parse Arguments
@@ -39,29 +39,31 @@ def main():
             user = dict(required=True),
             password = dict(required=True),
             address = dict(required=True),
-            query = dict(required=False),
+            query = dict(required=True),
+            gname = dict(required=True),
+            noremove = dict(default=False, required=False, type='bool')
         ),
     )
     user = module.params['user']
     password = module.params['password']
     address = module.params['address']
     query = module.params['query']
+    gname = module.params['gname']
+    noremove = module.params['noremove']
 
     ### Set Global Variable naviseccli
     global naviseccli
     naviseccli = "/opt/Navisphere/bin/naviseccli -user " + user + " -password " + password + " -address " + address + " -scope 0"
 
-    ### (Temporary) Set Storage Group
-    sg="TestMaskingSG"
-
     ### Remove Phase
-    removeHlus(parseQuery(query),sg) 
+    if not noremove:
+        removeHlus(parseQuery(query),gname)
 
     ### Add Phase
-    addHlus(parseQuery(query),sg)
+    addHlus(parseQuery(query),gname)
 
     res_args = dict(
-        changed = True, pair = getPairs(sg)
+        changed = True, pair = getPairs(gname)
     )
     module.exit_json(**res_args)
 
