c20bf83a3e69bdf4b7255bda8f4f6c75c8905c9b
hn826 <hn826@users.noreply.github.com>
2016-06-09 15:09:15 +0900

    Add function of Health Check & Bug Fix

---
+++ ansible-vnx-all/library/faultschk.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import commands
+import re
+
+from string import *
+from ansible.module_utils.basic import *
+
+def checkSp(user,password,spa,spb):
+    naviseccli_path = "/opt/Navisphere/bin/naviseccli"
+    (rc, out, err) = module.run_command('%s -user %s -password %s -address %s -scope 0 getarrayuid' % (naviseccli_path, user, password, spa))
+    if rc == 0:
+        return spa
+    else:
+        return spb
+
+def checkFaultslist():
+    (rc, out, err) = module.run_command('%s faults -list' % (naviseccli), check_rc=True)
+    if not re.search('The array is operating normally.', out):
+        module.fail_json(msg='Faults List Check Failed')
+        
+def main():
+    ### Parse Arguments
+    global module
+    module = AnsibleModule(
+        argument_spec = dict(
+            user = dict(required=True),
+            password = dict(required=True),
+            spa = dict(required=True),
+            spb = dict(required=True),
+        ),
+    )
+    user = module.params['user']
+    password = module.params['password']
+    spa = module.params['spa']
+    spb = module.params['spb']
+
+    ### Check SP
+    address = checkSp(user,password,spa,spb) 
+
+    ### Set Global Variable naviseccli
+    global naviseccli
+    naviseccli = "/opt/Navisphere/bin/naviseccli -user " + user + " -password " + password + " -address " + address + " -scope 0"
+
+    ### Check Faults List
+    checkFaultslist()
+
+    res_args = dict(
+        changed = False
+    )
+    module.exit_json(**res_args)
+
+if __name__ == '__main__':
+    main()
